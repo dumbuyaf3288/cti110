d7bb2346b6bf8942b06f5f5958b9b647d919c41d
dumbuyaf3288 <40256449+dumbuyaf3288@users.noreply.github.com>
2018-06-14 00:44:59 -0400

    first upload
    first upload

---
+++ P2T1_Salesprediction_DumbuyaFatmata.py
@@ -0,0 +1,13 @@
+# CTI-110
+# P2T1-Sales Predtion
+# Fatmata Dumbuya
+# 12th June, 2018
+
+# Get the projected total sales.
+total_sales = float(input ('enter the projected sales: '))
+
+# Calculate then profit aaas 23 percent of the total sales.
+profit = total_sales * 0.23
+
+# Display the profit.
+print('the profit is $', format(profit, ',.2f'))
